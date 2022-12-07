ca74b3df0f5ecf8ad58c6ca29fa2238ed92fdbb9
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2022-12-07 18:57:04 +0300

    Revizeler
    - Form Pop Up
    - Sayfa Revizeleri

--- php/footer.php
+++ php/footer.php
@@ -1,149 +1,201 @@
-<footer>
-        <div class="col-xl-12 d-flex mainBlur">
-            <div class="col-xl-4 half-drop"></div>
-            <div class="col-xl-8 full-drop ms-auto"></div>
-        </div>
-        <div class="container-fluid col-xl-10 drop">
-            <div class="row pt-5 pb-5">
-                <div class="col-xl-4 text-center text-xl-start mb-5">
-                    <h4 class="mb-5 ms-xl-5 ps-xl-1"><img class="me-4" src="assets/materials/post-ico.svg">How Can We Help?</h4>
-                    <div class="mini-box d-flex justify-content-center justify-content-xl-start mt-3">
-                        <a href="single-page.php">
-                            <div class="text-center mb me-3">
-                                <div class="incontent">
-                                    <img src="assets/materials/info.svg">
-                                    <p class="mt-3">How to buy!</p>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="single-page.php">
-                            <div class="text-center mb">
-                                <div class="incontent">
-                                    <img src="assets/materials/product-ico.svg">
-                                    <p class="mt-3">Product Support</p>
-                                </div>
-                            </div>
-                    </div>
-                    </a>
-                    <div class="mini-box d-flex justify-content-center justify-content-xl-start mt-3">
-                        <a href="mailto:info@refine-tr.com">
-                            <div class="text-center mb me-3">
-                                <div class="incontent">
-                                    <img src="assets/materials/mail.svg">
-                                    <p class="mt-3">E-Mail Sales</p>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="https://api.whatsapp.com/send?phone=+905000000000&text=Merhabalar,%20Refine%20Inc.%20ayr%C4%B1cal%C4%B1klar%C4%B1ndan%20yararlanmak%20istiyorum.">
-                            <div class="text-center mb">
-                                <div class="incontent">
-                                    <img src="assets/materials/chat.svg">
-                                    <p class="mt-3">Chat with Sales </p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-1 d-xl-auto d-none"></div>
-                <div class="col-xl-7 row p-xl-0 mx-auto ms-xl-auto mx-xl-0 my-auto">
-                    <div class="col-xl-4 mb-5 text-center text-xl-start">
-                        <h4 class="mb-3 d-xl-flex"><span class="order-1">Menu</span>
-                            <hr class="mx-xl-2 mx-auto" />
-                        </h4>
-                        <a href="about-us.php">
-                            <p>About Us</p>
-                        </a>
-                        <a href="prod-list.php">
-                            <p>Products</p>
-                        </a>
-                        <a href="single-page.php">
-                            <p>PCB Design & Manufacturing</p>
-                        </a>
-                        <a href="blog-page.php">
-                            <p>Blog</p>
-                        </a>
-                        <a href="contact.php">
-                            <p>Contact Us</p>
-                        </a>
-                    </div>
+ <!-- Login Area -->
+ <div class="container logMe">
+     <div class="row">
+         <button class="closer" onclick="closeForm()"><img src="assets/materials/x.svg"></button>
+         <div class="col-xl-5">
+             <div class="outflow d-flex align-content-center align-items-center justify-content-center">
+                 <div class="leftSide p-0 text-center row align-content-xl-between align-items-xl-between align-items-center align-content-center">
+                     <div>
+                         <img src="assets/materials/logo.svg" width="100px">
+                     </div>
+                     <div class="social d-xl-flex d-none justify-content-center">
+                         <a href="https://www.linkedin.com/company/refineinc/" class="me-3">
+                             <div class="sBox"><img src="assets/materials/linked-in.svg"></div>
+                         </a>
+                         <a href="#" class="me-3">
+                             <div class="sBox"><img src="assets/materials/insta.svg"></div>
+                         </a>
+                         <a href="#">
+                             <div class="sBox"><img src="assets/materials/youtube.svg"></div>
+                         </a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-xl-7 p-0">
+             <div class="outflow-half text-center d-flex align-content-center align-items-center justify-content-center">
+                 <div class="rightSide">
+                     <h1 class="mb-5 pb-xl-5">Reseller Login</h1>
+                     <form class="form mt-5" action="account.html">
+                         <div class="form-group">
+                             <input type="text" placeholder="E-Mail"><img src="assets/materials/env-mail.svg">
+                         </div>
+                         <div class="form-group mt-3">
+                             <input type="password" placeholder="Password"><img src="assets/materials/env-pw.svg">
+                         </div>
+                         <div class="form-group buttonArea mt-3 d-flex justify-content-between align-content-center align-items-center">
+                             <button type="submit" class="text-start ps-3">LOGIN</button>
+                             <div class="prefix"><img src="assets/materials/arrow-right.svg"></div>
+                         </div>
+                         <div class="form-group mt-3 mx-auto d-flex justify-content-center">
+                             <a href="#">Register Now</a>
+                         </div>
+                     </form>
+                 </div>
 
-                    <div class="col-xl-1 mb-5"></div>
+             </div>
 
-                    <div class="col-xl-7 mb-5 text-center text-xl-start">
-                        <h4 class="mb-3 d-xl-flex"><span class="order-1">Contact</span>
-                            <hr class="mx-xl-2 mx-auto" />
-                        </h4>
-                        <a target="blank_" href="https://g.page/refine-tr?share">
-                            <p>HQ: Kasap Sok. Gamze Apt. No:19/1 34394 Esentepe, Şişli, İstanbul, Türkiye</p>
-                        </a>
-                        <a href="#" target="blank_">
-                            <p>Warehouse I: Room 1202, 12/F Global Gateway (Tsuen Wan), 168 Yeung Uk Road, Tsuen Wan, Hong Kong</p>
-                        </a>
-                        <a href="#" target="blank_">
-                            <p>Warehouse II: No. 121 Tai Tao Tsuen Hung Shui Kiu Yuen Long, Hong Kong</p>
-                        </a>
-                        <div class="numberArea d-flex mt-5">
-                            <a href="tel:+90 850 433 87 60" class="d-flex justify-content-center justify-content-xl-start me-3 mb-2"><img src="assets/materials/phone.svg" width="24px">
-                                <p>+90 850 433 87 60</p>
-                            </a>
-                            <a href="mailto:info@refine-tr.com" class="d-flex justify-content-center justify-content-xl-start me-2 mb-2"><img src="assets/materials/mail.svg" width="24px">
-                                <p>info@refine-tr.com</p>
-                            </a>
-                        </div>
-                    </div>
+         </div>
+     </div>
+ </div>
+ <!-- Login Area -->
 
-                    <div class="col-xl-6 mt-auto text-center text-xl-start">
-                        <h4>Download PDF Documents</h4>
-                        <form class="form mt-4">
-                            <div class="form-group">
-                                <!-- <input type="text" placeholder="Your E-Mail Address"> -->
-                                <button type="submit"><img width="20px" style="margin-right: 10px;" src="assets/materials/pdf-fikirbuz.png" alt="">Datasheet</button>
-                            </div>
-                        </form>
-                    </div>
+ <footer>
+     <div class="col-xl-12 d-flex mainBlur">
+         <div class="col-xl-4 half-drop"></div>
+         <div class="col-xl-8 full-drop ms-auto"></div>
+     </div>
+     <div class="container-fluid col-xl-10 drop">
+         <div class="row pt-5 pb-5">
+             <div class="col-xl-4 text-center text-xl-start mb-5">
+                 <h4 class="mb-5 ms-xl-5 ps-xl-1"><img class="me-4" src="assets/materials/post-ico.svg">How Can We Help?</h4>
+                 <div class="mini-box d-flex justify-content-center justify-content-xl-start mt-3">
+                     <a href="tel:+908504338760 ">
+                         <div class="text-center mb me-3">
+                             <div class="incontent">
+                                 <img src="assets/materials/info.svg">
+                                 <p class="mt-3">How to buy!</p>
+                             </div>
+                         </div>
+                     </a>
+                     <a style="cursor: pointer;">
+                         <div class="text-center mb form">
+                             <div class="incontent">
+                                 <img src="assets/materials/product-ico.svg">
+                                 <p class="mt-3">Product Support</p>
+                             </div>
+                         </div>
+                 </div>
+                 </a>
+                 <div class="mini-box d-flex justify-content-center justify-content-xl-start mt-3">
+                     <a href="mailto:info@refine-tr.com">
+                         <div class="text-center mb me-3">
+                             <div class="incontent">
+                                 <img src="assets/materials/mail.svg">
+                                 <p class="mt-3">E-Mail Sales</p>
+                             </div>
+                         </div>
+                     </a>
+                     <a href="https://api.whatsapp.com/send?phone=+905000000000&text=Merhabalar,%20Refine%20Inc.%20ayr%C4%B1cal%C4%B1klar%C4%B1ndan%20yararlanmak%20istiyorum.">
+                         <div class="text-center mb">
+                             <div class="incontent">
+                                 <img src="assets/materials/chat.svg">
+                                 <p class="mt-3">Chat with Sales </p>
+                             </div>
+                         </div>
+                     </a>
+                 </div>
+             </div>
+             <div class="col-1 d-xl-auto d-none"></div>
+             <div class="col-xl-7 row p-xl-0 mx-auto ms-xl-auto mx-xl-0 my-auto">
+                 <div class="col-xl-4 mb-5 text-center text-xl-start">
+                     <h4 class="mb-3 d-xl-flex"><span class="order-1">Menu</span>
+                         <hr class="mx-xl-2 mx-auto" />
+                     </h4>
+                     <a href="about-us.php">
+                         <p>About Us</p>
+                     </a>
+                     <a href="prod-list.php">
+                         <p>Products</p>
+                     </a>
+                     <a href="single-page.php">
+                         <p>PCB Design & Manufacturing</p>
+                     </a>
+                     <a href="blog-page.php">
+                         <p>Blog</p>
+                     </a>
+                     <a href="contact.php">
+                         <p>Contact Us</p>
+                     </a>
+                 </div>
 
-                    <div class="col-xl-6 mt-auto ms-xl-auto mx-auto text-xl-end text-center">
-                        <div class="social d-flex justify-content-xl-end justify-content-center mt-4 mt-xl-auto">
-                            <a href="https://www.linkedin.com/company/refineinc/" target="blank_" class="me-3">
-                                <div class="sBox"><img src="assets/materials/linked-in.svg"></div>
-                            </a>
-                            <!-- <a href="#" target="blank_" class="me-3">
+                 <div class="col-xl-1 mb-5"></div>
+
+                 <div class="col-xl-7 mb-5 text-center text-xl-start">
+                     <h4 class="mb-3 d-xl-flex"><span class="order-1">Contact</span>
+                         <hr class="mx-xl-2 mx-auto" />
+                     </h4>
+                     <a target="blank_" href="https://g.page/refine-tr?share">
+                         <p>HQ: Kasap Sok. Gamze Apt. No:19/1 34394 Esentepe, Şişli, İstanbul, Türkiye</p>
+                     </a>
+                     <a href="#" target="blank_">
+                         <p>Warehouse I: Room 1202, 12/F Global Gateway (Tsuen Wan), 168 Yeung Uk Road, Tsuen Wan, Hong Kong</p>
+                     </a>
+                     <a href="#" target="blank_">
+                         <p>Warehouse II: No. 121 Tai Tao Tsuen Hung Shui Kiu Yuen Long, Hong Kong</p>
+                     </a>
+                     <div class="numberArea d-flex mt-5">
+                         <a href="tel:+90 850 433 87 60" class="d-flex justify-content-center justify-content-xl-start me-3 mb-2"><img src="assets/materials/phone.svg" width="24px">
+                             <p>+90 850 433 87 60</p>
+                         </a>
+                         <a href="mailto:info@refine-tr.com" class="d-flex justify-content-center justify-content-xl-start me-2 mb-2"><img src="assets/materials/mail.svg" width="24px">
+                             <p>info@refine-tr.com</p>
+                         </a>
+                     </div>
+                 </div>
+
+                 <div class="col-xl-6 mt-auto text-center text-xl-start">
+                     <h4>Download PDF Documents</h4>
+                     <form class="form mt-4">
+                         <div class="form-group">
+                             <!-- <input type="text" placeholder="Your E-Mail Address"> -->
+                             <button type="submit"><img width="20px" style="margin-right: 10px;" src="assets/materials/pdf-fikirbuz.png" alt="">Datasheet</button>
+                         </div>
+                     </form>
+                 </div>
+
+                 <div class="col-xl-6 mt-auto ms-xl-auto mx-auto text-xl-end text-center">
+                     <div class="social d-flex justify-content-xl-end justify-content-center mt-4 mt-xl-auto">
+                         <a href="https://www.linkedin.com/company/refineinc/" target="blank_" class="me-3">
+                             <div class="sBox"><img src="assets/materials/linked-in.svg"></div>
+                         </a>
+                         <!-- <a href="#" target="blank_" class="me-3">
                                 <div class="sBox"><img src="assets/materials/insta.svg"></div>
                             </a> -->
-                            <a href="https://www.youtube.com/channel/UCkyVtjw77RO3C6qRN1uDDXg" target="blank_">
-                                <div class="sBox"><img src="assets/materials/youtube.svg"></div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                         <a href="https://www.youtube.com/channel/UCkyVtjw77RO3C6qRN1uDDXg" target="blank_">
+                             <div class="sBox"><img src="assets/materials/youtube.svg"></div>
+                         </a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
 
-        <div class="container-fluid col-xl-10 subFooter">
-            <div class="row">
-                <div class="col-xl-6 text-xl-start text-center">
-                    <p>© Copyright <span id="year"></span> Refine Inc.</p>
-                </div>
-                <div class="col-xl-6 ms-auto text-xl-end text-center">
-                    <a href="single-page.php">Privacy</a>
-                    <a href="single-page.php">Terms of Use</a>
-                    <a href="single-page.php">Sitemap</a>
-                </div>
-            </div>
-        </div>
+     <div class="container-fluid col-xl-10 subFooter">
+         <div class="row">
+             <div class="col-xl-6 text-xl-start text-center">
+                 <p>© Copyright <span id="year"></span> Refine Inc.</p>
+             </div>
+             <div class="col-xl-6 ms-auto text-xl-end text-center">
+                 <a href="single-page.php">Privacy</a>
+                 <a href="single-page.php">Terms of Use</a>
+                 <a href="single-page.php">Sitemap</a>
+             </div>
+         </div>
+     </div>
 
-        <video playsinline autoplay muted loop poster="assets/materials/footer-bg.png" class="d-xl-block d-none">
-      <source src="assets/materials/footer-bg.webm" type="video/mp4">
-    </video>
-    </footer>
+     <video playsinline autoplay muted loop poster="assets/materials/footer-bg.png" class="d-xl-block d-none">
+         <source src="assets/materials/footer-bg.webm" type="video/mp4">
+     </video>
+ </footer>
 
 
 
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="assets/js/aos.js"></script>
-    <script src="assets/js/topbox.js"></script>
-    <script src="assets/js/main.js"></script>
+ <!-- Bootstrap JavaScript Libraries -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ <script src="assets/js/aos.js"></script>
+ <script src="assets/js/topbox.js"></script>
+ <script src="assets/js/main.js"></script>
