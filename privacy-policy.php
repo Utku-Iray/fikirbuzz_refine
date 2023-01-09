40183b9a882e7776cc3ef0e097fab265b44aa9b1
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2023-01-09 18:00:22 +0300

    gizlilik

---
+++ privacy-policy.php
@@ -0,0 +1,236 @@
+<?php
+require "database/connection.php";
+include "config.php";
+?>
+<!doctype html>
+<html lang="en">
+
+<?php include 'php/head.php' ?>
+
+<body dir="<?php if ($_SESSION['lang'] == "ar") echo "rtl";
+            else echo "ltr"; ?>">
+    <?php include 'php/header.php' ?>
+
+<style>
+    main .index-sec-1 li:hover{
+        background: none !important;
+        color: black;
+        cursor:auto;
+    }
+    main .index-sec-1 li{
+        height:60px !important;
+        margin-bottom: 30px;
+    }
+    p{
+        margin-bottom: 0rem !important;
+    }
+    h2 {
+        margin-bottom: 30px !important;
+    }
+    strong{
+        line-height: 28px;
+    }
+</style>
+
+    <main>
+
+        <div class="banner dark">
+            <div class="image-area">
+                <img class="bannerImg" src="assets/materials/contact-cover.png">
+            </div>
+            <div class="container-fluid col-xl-10">
+                <div class="row align-content-center align-items-center" style="min-height:200px !important">
+                    <div class="col-xl-7">
+
+                        <div class="title" data-aos="fade-right">
+                            <h1>PRIVACY POLICY</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="index-sec-1">
+            <div class="container">
+
+
+
+
+                <p><strong>This privacy policy explains how Refine Electronics Inc. (&lsquo;&lsquo;Refine&rsquo;&rsquo; or &lsquo;&lsquo;We&rsquo;&rsquo;) collects, uses, process, transfer your personal data in a variety of ways, when you:</strong></p>
+
+                <p><strong>(a) visit the website operated by Refine, or use the services provided on and through our website, including user manual, driver and whitepaper download, and/or</strong></p>
+
+                <p><strong>(b) use any Refine product, service, tool, software, or application on which We post a direct link to this privacy policy or which is referred to this privacy policy.</strong></p>
+
+                <p><strong>Refine is committed to protecting your personal data and being transparent about what information it holds. Refine understands its obligations to help you understand how and why Refine processes your personal data. This policy tells you about the why, what, how, and when in relation to Refine&rsquo;s collection and processing of your personal data.</strong></p>
+
+                <h2><strong>PURPOSE AND BASIS OF REFINE&rsquo;S COLLECTION AND PROCESSING OF YOUR PERSONAL DATA</strong></h2>
+
+                <p><strong>We collect and process your personal data for purpose of performing our obligations under the applicable agreement and/or purchase agreement with you.</strong></p>
+
+                <p><strong>We will use the personal data collected from you for purpose of, upon your consent, providing you with updated news and marketing information on the products and services offered by Refine. We also use your personal data to respond to you when you contact us.</strong></p>
+
+                <h2><strong>TYPE OF DATA REFINE COLLECTS ABOUT YOU</strong></h2>
+
+                <p><strong>The types of personal information we collect and process about you include:</strong></p>
+
+                <ul>
+                    <li>
+                        <p><strong>Your name and contact information (e.g., email, address, and phone number(s));</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Business information details such as information provided in the course of the contractual relationship between you and Refine, or otherwise voluntarily provided by you;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Information that is collected when you visit on of our website such as cookies;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Persistent identifier such as a customer number or other information held in a cookie or IP address; and</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Software usage statistics that is collected for compiling and analyzing the data to improve our products, software and services and to create personalized, positive user experiences; and</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Other information alike that helps fulfill the purpose for which it was collected.</strong></p>
+                    </li>
+                </ul>
+
+                <h2><strong>COOKIES</strong></h2>
+
+                <ol>
+                    <li>
+                        <p><strong>A &ldquo;Cookie&rdquo; is a small file, typically of letters and numbers, downloaded on to a device (such as desktop, laptop, tablet) when the user accesses certain websites. Cookies are then sent back to the originating website on each subsequent visit. Cookies are useful because they allow a website to recognize a user&rsquo;s device. The information below explains the cookies we use on our website and why we use them:</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>2. Cookies and similar technologies allow us to provide you with, for example, customized information from our Website and to make our Website more user-friendly through customer recognition, understanding and facilitation of the customer browsing and shopping behavior. This may include, for example, remembering your language selection so you don&rsquo;t have to re-enter it every time you visit our Website.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>In addition, cookies allow us to understand which pages you have visited and to determine how frequently particular pages are visited and to determine the most popular areas of our website. This enables us to improve our website apps to offer the best experience for you.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Our website also employs cookies and similar technologies placed by our carefully selected third party partners, including but not limited to Google and Zoho Sales IQ, to manage our commercial relationship with those partners.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>You can enable or disable cookies by modifying the settings in your browser. However, to make full use of our website, cookies need to be enabled on your browser or device. If you choose not to allow cookies, you will not be able to browse parts of our website. Without cookies you will not be able to place an order as cookies are required for the ordering process to be completed. They help, for example, to remember which and how many items you have selected to purchase.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>We have a variety of pages on social networking sites. If you use these pages, the hosting sites may store cookies onto your device. We do not control the setting of, and have no liability in connection with these cookies. Please look at the third-party websites for more information about the cookies they use and how you can manage them. Please read the terms of the applicable application for more information.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Third party cookies are those that are set by a domain different to the one that the user is visiting. Refine uses some carefully selected third parties to provide certain services to improve the service we offer to you. Some of these third parties use cookies. Information about how we use third parties and what cookies they use and why, is given below.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Refine also provides links to external third party websites and you may choose to click through to these sites. If you do so, these third party sites may send cookies to your browser. We do not control the setting of, and have no liability in connection with, these cookies. Please look at the cookies policies on the respective websites for more information about what they do with cookies and how you can manage them.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>Below list includes cookies deployed by such third parties on our Website.</strong></p>
+                    </li>
+                </ol>
+
+                <ul>
+                    <li>
+                        <p><strong>Google Analytics</strong></p>
+                    </li>
+                </ul>
+
+                <p><strong>We use Google Analytics to help us understand how our customers navigate to and through our Website. These cookies enable the function of Google analytics. This service helps us understand how long customers spend visiting different pages and how often they return to our Website. Google Analytics also helps our marketing colleagues to work out the effectiveness of our digital marketing campaigns. You can find out how to opt out of these cookies here:</strong></p>
+
+                <p><strong><a href="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a></strong></p>
+
+                <ul>
+                    <li>
+                        <p><strong>Zoho Sales IQ</strong></p>
+                    </li>
+                </ul>
+
+                <p><strong>Zoho SalesIQ, as a prospecting and engagement platform, starts tracking as soon as the visitor lands on a website with Zoho SalesIQ&#39;s code embedded in it. We have taken GDPR to heart in our development by including Privacy by Design in our product. It means you needn&#39;t worry about setting up all of your features to comply with GDPR, because they comply by default. The features that have undergone changes for GDPR compliance are listed below. \To know more information about Zoho SalesIQ, please see Cookie Policy:</strong></p>
+
+                <p><strong><a href="https://www.zoho.com/salesiq/gdpr.html">https://www.zoho.com/salesiq/gdpr.html</a></strong></p>
+
+                <h2><strong>SHARING OF YOUR PERSONAL INFORMATION</strong></h2>
+
+                <ol>
+                    <li>
+                        <p><strong>Refine is required to disclose certain personal data collected from you to third parties e.g. email delivery service provider(s), we have chosen to support us for carrying out the purposes stated above.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>With your prior consent, we may share information about you for publicity and marketing purposes online, in print and on social media.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>We reassure you that, Refine will not, under no circumstances, sell your personal data to third parties or permit third parties to sell the data we have shared with them.</strong></p>
+                    </li>
+                </ol>
+
+                <h2><strong>YOUR RIGHTS</strong></h2>
+
+                <p><strong>1. Right to withdraw consent. Where Refine is collecting or/and processing data based on your consent, you have the right to withdraw that consent at any time, however that you may not make full use of our websites if you withdraw your consents.</strong></p>
+
+                <p><strong>2. In addition, you have the right to:</strong></p>
+
+                <ul>
+                    <li>
+                        <p><strong>access your personal data;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>copy or transfer your personal data;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>demand rectification of incorrect or incomplete data;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>require deletion or removal of your personal data;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>request not to be subject to entirely automatic profiling algorithms;</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>impose restriction on processing of your personal data; and</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>object to the processing of your personal data based on grounds other than your express consent.</strong></p>
+                    </li>
+                </ul>
+
+                <p><strong>3. We may use automated processing with your personal data to allow us to provide you with service and product information that is most suited for your interests. If you are unhappy with the automated decision, you can contact us and request a manual review.</strong></p>
+
+                <h2><strong>SECURITY OF YOUR PERSONAL INFORMATION</strong></h2>
+
+                <ol>
+                    <li>
+                        <p><strong>Refine is committed to protecting the information you provide us. We take the information and system security very seriously and we strive to comply with our obligations at all times. Refine will take great care in maintaining the security of your personal data and in preventing unauthorized access to it through the use of appropriate technology and internal safeguards. Your information is protected by controls designed to minimize loss or damage through accident, negligence or deliberate actions.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>We suggest that you do not share your password information with anyone. If you are sharing a computer with others you should always choose to log out before leaving a site or service to protect access to your information from subsequent users.</strong></p>
+                    </li>
+                </ol>
+
+                <h2><strong>DATA RETENTION PERIOD</strong></h2>
+
+                <ol>
+                    <li>
+                        <p><strong>Except as otherwise required by applicable law, Refine endeavors to retain your personal data only for as long as is necessary for Refine to fulfill the aforementioned purposes. After the data retention period, we will destroy or delete your personal information or make it anonymous such that it cannot be identified or traced back to you.</strong></p>
+                    </li>
+                    <li>
+                        <p><strong>If in the future we intend to process your personal data for a purpose other than that which it was collected, we will notify you of that purpose together with any other relevant information.</strong></p>
+                    </li>
+                </ol>
+
+                <h2><strong>CHANGES TO THIS PRIVACY POLICY</strong></h2>
+
+                <p><strong>Refine may change the terms of the Privacy Policy from time to time. In case of any changes, we will post those changes. When we change the policy in a material way, a notice will be posted on our website along with the updated Privacy Policy. You may see the updated terms of Privacy Policy via Latest News.</strong></p>
+
+                <h2><strong>Contact Us</strong></h2>
+
+                <p><strong>If you have any concerns as to how your personal data is collected or processed or your rights, or any question about our Privacy Policy, you can contact us.</strong></p>
+
+                <p><br />
+                    &nbsp;</p>
+
+            </div>
+        </div>
+
+        <?php include 'php/footer.php' ?>
+</body>
+
+</html>
