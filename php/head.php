43d8e32ea28ca20b21f551e91090b711a052a717
Utku-Iray <51332027+Utku-Iray@users.noreply.github.com>
2023-02-02 20:03:10 +0300

    rev

--- php/head.php
+++ php/head.php
@@ -1,5 +1,18 @@
 
 <head>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-187296216-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-187296216-1');
+    </script>
+
     <title>Refine</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
